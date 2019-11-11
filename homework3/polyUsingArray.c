822af4f2af23c9b93d3d51725764a5e05aab7389
hgsong225 <themunt3@gmail.com>
2019-11-11 17:48:29 +0900

    sepraating functions

--- homework3/polyUsingArray.c
+++ homework3/polyUsingArray.c
@@ -23,144 +23,164 @@ struct poly
 
 clock_t start, finish, used_time = 0;    // variables to calulate running time.
 
+int add(int deg1, int deg2, struct poly* a, struct poly* b, struct poly* c) {
+    int i;
+    int m = 0;
+
+    if (deg1 > deg2)
+    {
+        for (i = 0; i <= deg2; i += 1)
+        {
+            c[m].coeff = a[i].coeff + b[i].coeff;
+            c[m].exp = a[i].exp;
+            m += 1;
+        }   
+        for (i = deg2 + 1; i <= deg1; i += 1)
+        {    
+            c[m].coeff = a[i].coeff;
+            c[m].exp = a[i].exp;
+            m += 1;
+        }  
+    }                              
+    else                   
+    {                                  
+        for (i = 0; i <= deg1; i += 1)
+        {                            
+            c[m].coeff = a[i].coeff + b[i].coeff;
+            c[m].exp = a[i].exp;
+            m += 1;
+        }          
+        for (i = deg1 + 1; i <= deg2; i += 1)
+        {    
+            c[m].coeff = b[i].coeff;
+            c[m].exp = b[i].exp;
+            m += 1;
+        }
+    }
+
+    return m;
+}
+
+int deduct(int deg1, int deg2, struct poly* a, struct poly* b, struct poly* d) {
+    int i;
+    int m = 0;
+
+    if (deg1 > deg2)
+    {
+        for (i = 0; i <= deg2; i += 1)
+        {
+            d[m].coeff = a[i].coeff - b[i].coeff;
+            d[m].exp = a[i].exp;
+            m += 1;
+        }   
+        for (i = deg2 + 1; i <= deg1; i += 1)
+        {    
+            d[m].coeff = a[i].coeff;
+            d[m].exp = a[i].exp;
+            m += 1;
+        }  
+    }                              
+    else                   
+    {                                  
+        for (i = 0; i <= deg1; i += 1)
+        {                            
+            d[m].coeff = a[i].coeff - b[i].coeff;
+            d[m].exp = a[i].exp;
+            m += 1;
+        }          
+        for (i = deg1 + 1; i <= deg2; i += 1)
+        {    
+            d[m].coeff = b[i].coeff;
+            d[m].exp = b[i].exp;
+            m += 1;
+        }
+    }
+
+    return m;
+}
+
+
 //declaration of polynomials
 struct poly a[50], b[50], c[50], d[50];
 
 void calcTime(void)
 {
     used_time = finish - start;
-    printf("\nALL DONE!\nTime: %f sec\n\n",(float)used_time/CLOCKS_PER_SEC);
+    printf("\nALL DONE!\nTime: %f sec\n\n", (float)used_time / CLOCKS_PER_SEC);
 }
 
 int main()
 {
     int i;
     int deg1, deg2;        //stores degrees of the polynomial
-    int k=0, l=0, m=0;
+    int k = 0, l = 0, m = 0;
 
     printf("Enter the highest degree of polynomial1:");
-    scanf("%d",&deg1);        //taking polynomial terms from the user
+    scanf("%d", &deg1);        //taking polynomial terms from the user
     for (i = 0; i <= deg1; i += 1)
     {
-       //entering values in coefficient of the polynomial terms
-       printf("\nEnter the coeff of x^%d :", i);
-       scanf("%f", &a[i].coeff);
-       //entering values in exponent of the polynomial terms
-       a[k++].exp = i;
-   }
+        //entering values in coefficient of the polynomial terms
+        printf("\nEnter the coeff of x^%d :", i);
+        scanf("%f", &a[i].coeff);
+        //entering values in exponent of the polynomial terms
+        a[k++].exp = i;
+    }
 
-   //taking second polynomial from the user
-   printf("\nEnter the highest degree of polynomial2:");
-   scanf("%d", &deg2);
-   for (i = 0; i <= deg2; i += 1)
-   {
-       printf("\nEnter the coeff of x^%d :", i);
-       scanf("%f", &b[i].coeff);
-       b[l++].exp = i;
-   }
-   //printing first polynomial
-   printf("\nExpression 1 = %.1f", a[0].coeff);
-   for (i = 1; i <= deg1; i += 1)
-   {
-       printf(" + %.1fx^%d", a[i].coeff, a[i].exp);
-   }
-   //printing second polynomial
-   printf("\nExpression 2 = %.1f", b[0].coeff);
-   for (i = 1; i <= deg2; i += 1)
-   {    
+    //taking second polynomial from the user
+    printf("\nEnter the highest degree of polynomial2:");
+    scanf("%d", &deg2);
+    for (i = 0; i <= deg2; i += 1)
+    {
+        printf("\nEnter the coeff of x^%d :", i);
+        scanf("%f", &b[i].coeff);
+        b[l++].exp = i;
+    }
+
+    //printing first polynomial
+    printf("\nExpression 1 = %.1f", a[0].coeff);
+    for (i = 1; i <= deg1; i += 1)
+    {
+        printf(" + %.1fx^%d", a[i].coeff, a[i].exp);
+    }
+    //printing second polynomial
+    printf("\nExpression 2 = %.1f", b[0].coeff);
+    for (i = 1; i <= deg2; i += 1)
+    {    
         printf(" + %.1fx^%d", b[i].coeff, b[i].exp);
-   }
+    }
 
 
-   start = clock();
+    start = clock();
 
-   //Adding the polynomials
-   if (deg1 > deg2)
-   {
-       for (i = 0; i <= deg2; i += 1)
-       {
-           c[m].coeff = a[i].coeff + b[i].coeff;
-           c[m].exp = a[i].exp;
-           m += 1;
-       }   
-       for (i = deg2 + 1; i <= deg1; i += 1)
-       {    
-           c[m].coeff = a[i].coeff;
-           c[m].exp = a[i].exp;
-           m += 1;
-       }  
-   }                              
-   else                   
-   {                                  
-       for (i = 0; i <= deg1; i += 1)
-       {                            
-           c[m].coeff = a[i].coeff + b[i].coeff;
-           c[m].exp = a[i].exp;
-           m += 1;
-       }          
-       for (i=deg1+1;i<=deg2;i++)
-       {    
-           c[m].coeff = b[i].coeff;
-           c[m].exp = b[i].exp;
-            m += 1;
-       }
-   }
+    //Adding the polynomials
+    m = add(deg1, deg2, a, b, c);
 
     finish = clock();
     calcTime();
 
-   //printing the sum of the two polynomials
-   printf("\nExpression after addition = %.1f", c[0].coeff);
-   for (i = 1; i < m; i += 1)
-   {
-       printf(" + %.1fx^%d", c[i].coeff, c[i].exp);
-   }
+    //printing the sum of the two polynomials
+    printf("\nExpression after addition = %.1f", c[0].coeff);
+    for (i = 1; i < m; i += 1)
+    {
+        printf(" + %.1fx^%d", c[i].coeff, c[i].exp);
+    }
 
-   m = 0;
+    m = 0;
 
-   start = clock();
+    start = clock();
 
-   //deducting the polynomials
-   if (deg1 > deg2)
-   {
-       for (i = 0; i <= deg2; i += 1)
-       {
-           d[m].coeff = a[i].coeff - b[i].coeff;
-           d[m].exp = a[i].exp;
-           m += 1;
-       }   
-       for (i = deg2 + 1; i <= deg1; i += 1)
-       {    
-           d[m].coeff = a[i].coeff;
-           d[m].exp = a[i].exp;
-           m += 1;
-       }  
-   }                              
-   else                   
-   {                                  
-       for (i = 0; i <= deg1; i += 1)
-       {                            
-           d[m].coeff = a[i].coeff - b[i].coeff;
-           d[m].exp = a[i].exp;
-           m += 1;
-       }          
-       for (i=deg1+1;i<=deg2;i++)
-       {    
-           c[m].coeff = b[i].coeff;
-           c[m].exp = b[i].exp;
-            m += 1;
-       }
-   }
+    //deducting the polynomials
+    m = deduct(deg1, deg2, a, b, d);
 
     finish = clock();
     calcTime();
    
-   //printing the result of the two polynomials
-   printf("\nExpression after deduction = %.1f", d[0].coeff);
-   for (i = 1; i < m; i += 1)
-   {
-       printf(" + %.1fx^%d", d[i].coeff, d[i].exp);
-   }
+    //printing the result of the two polynomials
+    printf("\nExpression after deduction = %.1f", d[0].coeff);
+    for (i = 1; i < m; i += 1)
+    {
+        printf(" + %.1fx^%d", d[i].coeff, d[i].exp);
+    }
 
     return 0;
 }
